1c59f0ffbf670ba39591bc2bc12b26490e4a8478
dangen-effy <dangen.effy@gmail.com>
2024-01-12 16:28:25 +0900

    fix: adquality on android

--- packages/ironsource_adqualitysdk/example/android/app/src/main/kotlin/com/example/ironsource_adqualitysdk_example/MainActivity.kt
+++ packages/ironsource_adqualitysdk/example/android/app/src/main/kotlin/com/example/ironsource_adqualitysdk_example/MainActivity.kt
@@ -1,28 +1,6 @@
-package com.example.ironsource_adqualitysdk_example
+package com.example.awesome_example_example
 
-import android.util.Log
-import com.ironsource.adqualitysdk.sdk.IronSourceAdQuality
-import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
 
-private val CHANNEL = "io.heybit.bitbunny/ironsource_adqualitysdk"
-
-class MainActivity : FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
-                call,
-                result ->
-            when (call.method) {
-                "initializeIronSource" -> {
-                    val appKey = call.argument<String>("appKey") ?: return@setMethodCallHandler
-                    IronSourceAdQuality.getInstance().initialize(this, appKey)
-                    result.success("IronSourceAdQuality initialized")
-                }
-                else -> result.notImplemented()
-            }
-        }
-    }
+class MainActivity: FlutterActivity() {
 }
