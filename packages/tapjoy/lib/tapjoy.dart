f54968559f9ebeb90800d7459df0bdb5c760e243
Andy Lee <andyleeboo9@icloud.com>
2024-09-27 15:09:28 +0900

    feat: Tapjoy 충돌 해소를 위해 안드로이드에서만 플러그인 제공

--- packages/tapjoy/lib/tapjoy.dart
+++ packages/tapjoy/lib/tapjoy.dart
@@ -1,19 +1,33 @@
+import 'dart:io';
+
 import 'tapjoy_platform_interface.dart';
 
 class Tapjoy {
-  Future<String?> setUserId(String userId) {
-    return TapjoyPlatform.instance.setUserId(userId);
+  Future<String?> setUserId(String userId) async {
+    if (Platform.isAndroid) {
+      return TapjoyPlatform.instance.setUserId(userId);
+    }
+    return Future.value(null); // No-op for iOS
   }
 
-  Future<String?> connect(String sdkKey) {
-    return TapjoyPlatform.instance.connect(sdkKey);
+  Future<String?> connect(String sdkKey) async {
+    if (Platform.isAndroid) {
+      return TapjoyPlatform.instance.connect(sdkKey);
+    }
+    return Future.value(null); // No-op for iOS
   }
 
-  Future<String?> getPlacement() {
-    return TapjoyPlatform.instance.getPlacement();
+  Future<String?> getPlacement() async {
+    if (Platform.isAndroid) {
+      return TapjoyPlatform.instance.getPlacement();
+    }
+    return Future.value(null); // No-op for iOS
   }
 
-  Future<String?> showContent() {
-    return TapjoyPlatform.instance.showContent();
+  Future<String?> showContent() async {
+    if (Platform.isAndroid) {
+      return TapjoyPlatform.instance.showContent();
+    }
+    return Future.value(null); // No-op for iOS
   }
 }
